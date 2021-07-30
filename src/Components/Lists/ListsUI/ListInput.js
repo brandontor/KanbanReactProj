79fa7bed22470acb38b0ab7b57e8443085a2b4f2
brandontor <53887678+brandontor@users.noreply.github.com>
2021-07-30 16:33:16 -0400

    added styling for input text areas

--- src/Components/Lists/ListsUI/ListInput.js
+++ src/Components/Lists/ListsUI/ListInput.js
@@ -12,7 +12,7 @@ const ListInput = ({inputVisibility, cardText, setNewCardText}) => {
             <form className="col s12" id={inputVisibility} style={{display: `${inputVisibility}`}}>
                     <div className="input-field col s6">
                         <i className="material-icons prefix" id="text-icon">mode_edit</i>
-                        <textarea className="text-field" id="text-field" onChange={cardInputHandler} value={cardText}></textarea>
+                        <textarea className="text-field" id="text-field" onChange={cardInputHandler} value={cardText} placeholder='Set Card Name'></textarea>
                     </div>
             </form>
     );
