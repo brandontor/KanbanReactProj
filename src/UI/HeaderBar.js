d9075625cf9a76cf1afb07917190f6a31e284fda
brandontor <53887678+brandontor@users.noreply.github.com>
2021-08-03 22:16:35 -0400

    added link to github page

--- src/UI/HeaderBar.js
+++ src/UI/HeaderBar.js
@@ -6,6 +6,7 @@ const HeaderBar = () => {
 
     <div className="nav-wrapper mainheader">
       <h1 className="brand-logo">Brello<i className="fab fa-angellist"></i></h1>
+      <a className="github" href="https://github.com/brandontor/KanbanReactProj"><p>Read Me <i className="fab fa-github"></i></p></a>
     </div>
 
   );
